2d71ba5942038c692122ec4c29a7f07689947d34
Stanislav <meninnik@gmail.com>
2020-03-22 20:56:47 +0200

    Prev button added.

--- app/src/main/java/com/javernaut/gquiz/MainActivity.java
+++ app/src/main/java/com/javernaut/gquiz/MainActivity.java
@@ -7,15 +7,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 public class MainActivity extends LoggingActivity {
 
     private static final String KEY_CURRENT_QUESTION_INDEX = "key_current_question_index";
+    private static final String KEY_ANSWERED_QUESTION_ARRAY = "key_answeredQuestion_Array";
+    private static final String KEY_ANSWERED_QUESTION_COUNT = "KEY_ANSWERED_QUESTION_COUNT";
+    private static final String KEY_ANSWERED_QUESTION_CORRECT_COUNT = "KEY_ANSWERED_QUESTION_CORRECT_COUNT";
 
     private Button trueButton;
     private Button falseButton;
     private Button nextButton;
+    private Button checktButton;
     private TextView questionView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -28,6 +31,9 @@ public class MainActivity extends LoggingActivity {
     };
 
     private int currentQuestionIndex = 0;
+    private int answeredCount = 0;
+    private int answeredCorrectCount = 0;
+    private int[] answeredQuestion = new int[mQuestionBank.length];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,11 +42,15 @@ public class MainActivity extends LoggingActivity {
 
         if (savedInstanceState != null) {
             currentQuestionIndex = savedInstanceState.getInt(KEY_CURRENT_QUESTION_INDEX);
+            answeredQuestion = savedInstanceState.getIntArray(KEY_ANSWERED_QUESTION_ARRAY);
+            answeredCount = savedInstanceState.getInt(KEY_ANSWERED_QUESTION_COUNT);
+            answeredCorrectCount = savedInstanceState.getInt(KEY_ANSWERED_QUESTION_CORRECT_COUNT);
         }
 
         trueButton = findViewById(R.id.true_button);
         falseButton = findViewById(R.id.false_button);
         nextButton = findViewById(R.id.next_button);
+        checktButton = findViewById(R.id.check_button);
         questionView = findViewById(R.id.question);
 
         applyCurrentQuestion();
@@ -73,12 +83,23 @@ public class MainActivity extends LoggingActivity {
                 applyCurrentQuestion();
             }
         });
+
+        checktButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        String.format("Отвечено %d/%d вопросов\n Правильных ответов: %d", answeredCount, answeredQuestion.length, answeredCorrectCount), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt(KEY_CURRENT_QUESTION_INDEX, currentQuestionIndex);
+        outState.putIntArray(KEY_ANSWERED_QUESTION_ARRAY, answeredQuestion);
+        outState.putInt(KEY_ANSWERED_QUESTION_COUNT, answeredCount);
+        outState.putInt(KEY_ANSWERED_QUESTION_CORRECT_COUNT, answeredCorrectCount);
     }
 
     private void applyCurrentQuestion() {
@@ -91,7 +112,16 @@ public class MainActivity extends LoggingActivity {
 
     private void onAnswerSelected(boolean currentAnswer) {
         boolean wasTheAnswerCorrect = currentAnswer == getCurrentQuestion().getCorrectAnswer();
-
+        if (answeredQuestion[currentQuestionIndex] == 0 ) {
+            answeredCount++;
+        }
+        if (wasTheAnswerCorrect && answeredQuestion[currentQuestionIndex] < 1){
+            answeredCorrectCount++;
+        }
+        if (!wasTheAnswerCorrect && answeredQuestion[currentQuestionIndex] == 1){
+            answeredCorrectCount--;
+        }
+        answeredQuestion[currentQuestionIndex] = (wasTheAnswerCorrect ? 1 : -1);
         showToast(wasTheAnswerCorrect ? R.string.correct_toast : R.string.incorrect_toast);
     }
 
