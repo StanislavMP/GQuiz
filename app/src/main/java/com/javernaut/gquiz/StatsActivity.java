d24df240e25810904fe53e2f5e60a48950b60dfd
Stanislav <meninnik@gmail.com>
2020-03-27 18:20:32 +0200

    More Activity

--- app/src/main/java/com/javernaut/gquiz/StatsActivity.java
+++ app/src/main/java/com/javernaut/gquiz/StatsActivity.java
@@ -20,9 +20,9 @@ public class StatsActivity extends LoggingActivity {
         statsView.setText(getIntent().getStringExtra(KEY_STATS_TEXT));
     }
 
-    public static Intent makeIntent(Context context, String string) {
+    public static Intent makeIntent(Context context, int answeredCount, int questionCount, int answeredCorrectCount) {
         Intent intent = new Intent(context, StatsActivity.class);
-        intent.putExtra(KEY_STATS_TEXT, string);
+        intent.putExtra(KEY_STATS_TEXT, String.format("Отвечено %d/%d вопросов\n Правильных ответов: %d", answeredCount, questionCount, answeredCorrectCount));
         return intent;
     }
 }
