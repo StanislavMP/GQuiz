c7c0e630aa8228b7fd2c79fd7026a3d88c897308
Stanislav <meninnik@gmail.com>
2020-04-07 16:48:42 +0300

    Fragment

---
+++ app/src/main/java/com/javernaut/gquiz/QuestionsFragment.java
@@ -0,0 +1,130 @@
+package com.javernaut.gquiz;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class QuestionsFragment extends Fragment {
+
+    private static final String KEY_CURRENT_QUESTION_INDEX = "key_current_question_index";
+
+    private Button trueButton;
+    private Button falseButton;
+    private Button nextButton;
+    private TextView questionView;
+
+    private Question[] mQuestionBank = new Question[] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
+    };
+
+    private int currentQuestionIndex = 0;
+
+    public QuestionsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_questions, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        trueButton = view.findViewById(R.id.true_button);
+        falseButton = view.findViewById(R.id.false_button);
+        nextButton = view.findViewById(R.id.next_button);
+        questionView = view.findViewById(R.id.question);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            currentQuestionIndex = savedInstanceState.getInt(KEY_CURRENT_QUESTION_INDEX);
+        }
+
+        applyCurrentQuestion();
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(true);
+            }
+        });
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onAnswerSelected(false);
+            }
+        });
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // move to next question
+                if (currentQuestionIndex == mQuestionBank.length - 1) {
+                    currentQuestionIndex = 0;
+                } else {
+                    currentQuestionIndex++;
+                }
+
+                // apply question
+                applyCurrentQuestion();
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_CURRENT_QUESTION_INDEX, currentQuestionIndex);
+    }
+
+    private void applyCurrentQuestion() {
+        questionView.setText(getCurrentQuestion().getQuestionResId());
+    }
+
+    private Question getCurrentQuestion() {
+        return mQuestionBank[currentQuestionIndex];
+    }
+
+    private void onAnswerSelected(boolean currentAnswer) {
+        boolean wasTheAnswerCorrect = currentAnswer == getCurrentQuestion().getCorrectAnswer();
+
+        showToast(wasTheAnswerCorrect ? R.string.correct_toast : R.string.incorrect_toast);
+    }
+
+    private void showToast(int textId) {
+        Toast.makeText(getView().getContext(), textId, Toast.LENGTH_SHORT).show();
+    }
+
+    public static QuestionsFragment makeInstance() {
+        QuestionsFragment fragment = new QuestionsFragment();
+        return fragment;
+    }
+}
